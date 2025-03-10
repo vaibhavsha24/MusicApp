77e900bec9f2c1f569a779b35245fcb16093d325
vaibhav-probo <155969836+vaibhav-probo@users.noreply.github.com>
2025-03-10 08:34:48 +0530

    api changed to implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ dependencies {
     implementation ("com.squareup.okhttp3:okhttp:4.10.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    api("com.google.dagger:hilt-android:2.51.1")
 
+    implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-work:1.2.0")
     kapt("androidx.hilt:hilt-compiler:1.2.0")
